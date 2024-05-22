3df238784529c9819adbc2dcf80c619cf3bfc239
Bruno Carneiro <bruno@zapt.tech>
2024-05-22 08:05:21 -0300

    Updating Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,13 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "ZaptLocationSDKSwiftFramework",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "ZaptLocationSDKSwiftFramework",
