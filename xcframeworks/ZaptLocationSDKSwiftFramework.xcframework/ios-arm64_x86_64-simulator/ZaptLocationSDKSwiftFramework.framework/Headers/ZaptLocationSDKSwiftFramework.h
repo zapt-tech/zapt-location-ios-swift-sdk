da0bca1ede57efb3137bd12ddd174c48695e952c
Bruno Carneiro <bruno@zapt.tech>
2025-09-19 15:38:18 -0300

    adding beacon listeners

---
+++ xcframeworks/ZaptLocationSDKSwiftFramework.xcframework/ios-arm64_x86_64-simulator/ZaptLocationSDKSwiftFramework.framework/Headers/ZaptLocationSDKSwiftFramework.h
@@ -0,0 +1,17 @@
+//
+//  ZaptLocationSDKSwiftFramework.h
+//  ZaptLocationSDKSwiftFramework
+//
+//  Created by Bruno Carneiro on 22/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZaptLocationSDKSwiftFramework.
+FOUNDATION_EXPORT double ZaptLocationSDKSwiftFrameworkVersionNumber;
+
+//! Project version string for ZaptLocationSDKSwiftFramework.
+FOUNDATION_EXPORT const unsigned char ZaptLocationSDKSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZaptLocationSDKSwiftFramework/PublicHeader.h>
+
